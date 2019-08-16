bcff1517925d9a2273b7aa6888e3250f0f672b15
rjw1428 <rjw1428@gmail.com>
2019-08-16 06:02:18 -0400

    Lesson 100 - Returning multiple JSX components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import styles from './Person.css';
-
+import Auxil from '../../hoc/Auxil'
 class Person extends Component {
     render() {
         console.log(this.props.name + " is rendering...")
         return (
-            <div className={styles.Person}>
-                <p onClick={this.props.click}>My name is this.{this.props.name} and I am {Math.floor(Math.random() * 30)} ({this.props.age}) years old</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </div>
-        )
+            <Auxil>
+                <p key="el1" onClick={this.props.click}>My name is this.{this.props.name} and I am {Math.floor(Math.random() * 30)} ({this.props.age}) years old</p>,
+                <p key="el2">{this.props.children}</p>,
+                <input key="el3" type="text" onChange={this.props.changed} value={this.props.name}></input>,
+            </Auxil>)
     };
 }
 
