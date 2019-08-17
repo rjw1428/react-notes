70d760a18511596624ff3e9ce5dccdfd0def1a12
rjw1428 <rjw1428@gmail.com>
2019-08-17 12:54:48 -0400

    Lesson 108 - Using Refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,13 +5,27 @@ import withClass from '../../hoc/WithClass'
 import PropTypes from 'prop-types'
 
 class Person extends Component {
+    constructor(props) {
+        super(props)
+        this.inputElementRef = React.createRef();
+    }
+    componentDidMount() {
+        // this.inputElementRef.focus()
+        this.inputElementRef.current.focus()
+    }
     render() {
         console.log(this.props.name + " is rendering...")
         return (
             <Auxil>
                 <p key="el1" onClick={this.props.click}>My name is this.{this.props.name} and I am {Math.floor(Math.random() * 30)} ({this.props.age}) years old</p>
                 <p key="el2">{this.props.children}</p>
-                <input key="el3" type="text" onChange={this.props.changed} value={this.props.name}></input>
+                <input 
+                key="el3"
+                // ref={(inputEl) => {this.inputElement = inputEl}}
+                ref={this.inputElementRef}
+                type="text" 
+                onChange={this.props.changed} 
+                value={this.props.name} />
             </Auxil>)
     };
 }
